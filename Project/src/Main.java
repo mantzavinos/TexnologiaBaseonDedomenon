cd01d612e5a55ac294b35a421fd723de2ca76e2a
Thanasis Mantzavinos <mantzavi@csd.auth.gr>
2021-07-16 19:31:26 +0300

    Second commit. Just adding the main class.

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,2 +1,10 @@
+/**
+ * Project for Texnologia baseon dedomenon.
+ * Created by:
+ */
 public class Main {
+    public static void main(String[] args) throws Exception{
+        Menu menu= new Menu();
+        menu.start();
+    }
 }
--- Project/src/Menu.java
+++ Project/src/Menu.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * The class of the menu of the program.
+ */
 public class Menu {
+    public void start() throws Exception{
+
+    }
+
 }
